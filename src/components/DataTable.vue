ca1e9b4da6c8f1007d6f438f11e020d4abf2d4e7
Stanislav Matulis <sv123.s@yandex.ru>
2022-04-28 21:08:13 +0300

    Реализация таблицы с данными

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -3,8 +3,49 @@
     <div class="data-table__filter">
       <ui-money v-model="moneyFilter" />
     </div>
-
-    <!-- Your component code here -->
+    <div class="data-table__head">
+      <div class="data-table__row data-table__row_not-bordered-btm">
+        <div
+          v-for="(column, i) in columns"
+          :key="column.prop"
+          class="data-table__cell data-table__cell_strong"
+          :style="getCellStyles(i)">
+            {{ column.label }}
+        </div>
+      </div>
+    </div>
+    <div class="data-table__body">
+      <div
+        v-for="row in currentPageRows"
+        :key="row.id"
+        class="data-table__row"
+      >
+        <div class="data-table__cell data-table__cell_strong data-table__cell_visible-xs">
+          {{ columns[0].label }}
+        </div>
+        <div class="data-table__cell" :style="getCellStyles(0)">
+          {{ row.id }}
+        </div>
+        <div class="data-table__cell data-table__cell_strong data-table__cell_visible-xs">
+          {{ columns[1].label }}
+        </div>
+        <div class="data-table__cell" :style="getCellStyles(1)">
+          {{ row.date }}
+        </div>
+        <div class="data-table__cell data-table__cell_strong data-table__cell_visible-xs">
+          {{ columns[2].label }}
+        </div>
+        <div class="data-table__cell" :style="getCellStyles(2)">
+          {{ row.name }}
+        </div>
+        <div class="data-table__cell data-table__cell_strong data-table__cell_visible-xs">
+          {{ columns[3].label }}
+        </div>
+        <div class="data-table__cell" :style="getCellStyles(3)">
+          {{ row.money }}
+        </div>
+      </div>
+    </div>
 
     <div class="data-table__paginator">
       <ui-pagination
@@ -40,6 +81,19 @@ export default {
   computed: {
     pageCount() {
       return Math.ceil(this.rows.length / this.pageSize);
+    },
+
+    currentPageRows() {
+      const start = (this.page - 1) * 4;
+      return this.rows.slice(start, start + this.pageSize);
+    },
+  },
+
+  methods: {
+    getCellStyles(index) {
+      return {
+        flexBasis: this.columns[index].width,
+      };
     },
   },
 };
